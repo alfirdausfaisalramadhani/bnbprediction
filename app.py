73758767a2bb66256ebee635a8fb643011753401
Alfirdaus Faisal Ramadhani <64518526+alfirdausfaisalramadhani@users.noreply.github.com>
2022-07-11 19:48:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data, model # import your app modules here
+from apps import home, data, model
 
 app = MultiApp()
 
 st.header('PREDIKSI HARGA COIN BNB-USD')
 
-
-# Add all your application here
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 st.markdown("""
@@ -19,11 +17,4 @@ st.markdown("""
 app.add_app("BERANDA", home.app)
 app.add_app("PREDIKSI", data.app)
 app.add_app("VIDEO SEPUTAR BINANCE", model.app)
-# The main app
 app.run()
-
-# st.markdown("""
-# <footer class="footer bg-danger py-4">
-#   <div class="container"><div class=" text-center text-white">Alfirdaus Faisal Ramadhani - Copyright©2022</div></div>
-# </footer>
-# """, unsafe_allow_html=True)

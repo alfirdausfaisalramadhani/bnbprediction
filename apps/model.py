478fed143561872b9c432072bf64c72088b7f9b7
Alfirdaus Faisal Ramadhani <64518526+alfirdausfaisalramadhani@users.noreply.github.com>
2022-07-15 20:58:53 +0700

    Update model.py

--- apps/model.py
+++ apps/model.py
@@ -1,4 +1,8 @@
 import streamlit as st
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 
 def app():
 
